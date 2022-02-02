5751ee745557efa0670544481e35d7b2cb7e1e99
Glib Petrachkov <petrasha@gmail.com>
2022-02-02 17:04:25 +0200

    steps 3 and 4

--- Hometask_31_Jan_Figures/Hometask_31_Jan_Figures.cpp
+++ Hometask_31_Jan_Figures/Hometask_31_Jan_Figures.cpp
@@ -30,60 +30,115 @@
 
 //2. Вывести на экран каркас параллелепипеда размерностью AxBxC.
 
-#include <iostream>
-#include <Windows.h>
-using namespace std;
-int main()
-{
-	    int width = 19;
-		int height = 19;
-		int depth = 10;
-	    char symbol = 178;
-	    for (int y = 0; y < height; y++)
-	    {
-	        HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-	
-	        for (int x = 0; x < width; x++)
-	        {
-	            
-	             if (x == 1 && y <= 10 && y >= 5 || x == 9 && y <= 10 && y >= 5 || x == 4 && y <= 7 && y >= 2 || x == 12 && y <= 7 && y >= 2)
-	            {
-	                 
-	                SetConsoleTextAttribute(h, 12); // light blue diagonals
-	
-	            }
-				 else if (y == 7 && x <= 11 && x >= 5 || y == 2 && x <= 11 && x >= 5 || y == 10 && x <= 8 && x >= 2 || y == 5 && x <= 8 && x >= 2)
-				{
+//#include <iostream>
+//#include <Windows.h>
+//using namespace std;
+//int main()
+//{
+//	    int width = 19;
+//		int height = 10;
+//		int depth = 10;
+//	    char symbol = 178;
+//	    for (int y = 0; y < height; y++)
+//	    {
+//	        HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+//	
+//			
+//	        for (int x = 0; x < width; x++)
+//	        {				      
+//	           if (x == 4 && y <= 7 && y >= 0)
+//	            {	               
+//	               SetConsoleTextAttribute(h, 12); // light blue diagonals
+//	            }
+//				 else if (y == 0 && x <= 11 && x >= 5 )
+//				{
+//					 SetConsoleTextAttribute(h, 11); // light blue diagonals
+//				}
+//				 else if (y == width / 2 - x - 5 && x<= 5 && x >= 0)
+//				 {
+//					 SetConsoleTextAttribute(h, 10); // light blue diagonals
+//				 }
+//	            else
+//	            {	               
+//	                SetConsoleTextAttribute(h, 9); // blue triangles
+//		        }
+//	            cout << symbol << symbol;
+//	        }
+//	        cout << "\n";
+//		}
+//	}
 
-					 SetConsoleTextAttribute(h, 11); // light blue diagonals
-
-				}
-				 else if (y == width / 2 - x -3 && x<= 4 && x >= 1 || y == width / 2 - x + 2 && x <= 4 && x >= 1 || y == width / 2 - x + 5 && x <= 12 && x >= 9 || y == width / 2 - x +10 && x <= 12 && x >= 9)
-				 {
-					 SetConsoleTextAttribute(h, 10); // light blue diagonals
-				 }
-	           
-	            else
-	            {
-	                 
-	                SetConsoleTextAttribute(h, 9); // blue triangles
-		            }
-	            cout << symbol << symbol;
-	        }
-	        cout << "\n";
-		    }
-	}
-
+//#include <iostream>
+//#include <Windows.h>
+//using namespace std;
+//int main()
+//{
+//	int width = 19;
+//	int height = 19;
+//	int depth = 10;
+//	char symbol = 178;
+//	for (int y = 0; y < height; y++)
+//	{
+//		HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+//
+//		for (int x = 0; x < width; x++)
+//		{
+//
+//			if (x == 1 && y <= 10 && y >= 5 || x == 9 && y <= 10 && y >= 5 || x == 4 && y <= 7 && y >= 2 || x == 12 && y <= 7 && y >= 2)
+//			{
+//				SetConsoleTextAttribute(h, 12); // light blue diagonals
+//			}
+//			else if (y == 7 && x <= 11 && x >= 5 || y == 2 && x <= 11 && x >= 5 || y == 10 && x <= 8 && x >= 2 || y == 5 && x <= 8 && x >= 2)
+//			{
+//				SetConsoleTextAttribute(h, 11); // light blue diagonals
+//			}
+//			else if (y == width / 2 - x - 3 && x <= 4 && x >= 1 || y == width / 2 - x + 2 && x <= 4 && x >= 1 || y == width / 2 - x + 5 && x <= 12 && x >= 9 || y == width / 2 - x + 10 && x <= 12 && x >= 9)
+//			{
+//				SetConsoleTextAttribute(h, 10); // light blue diagonals
+//			}
+//			else
+//			{
+//				SetConsoleTextAttribute(h, 9); // blue triangles
+//			}
+//			cout << symbol << symbol;
+//		}
+//		cout << "\n";
+//	}
+//}
 
 
 //3. Вывести лестницу из отрезков определённой длины.Длина(например, 14) и количество ступенек(например, 4) указывается с клавиатуры.
-//* *************
-//**************
-//**************
-//**************
 
-//4. Показать на экране лесенку. Количество ступенек(например, 5) указывает пользователь.
-//
+//#include <iostream>
+//#include <Windows.h>
+//using namespace std;
+//int main()
+//{
+//    int width = 35;
+//    int height = 10;
+//    cout << "Enter the number of steps - ";
+//    cin >> height; 
+//    int z;
+//    cout << "Enter the size of the step - "; 
+//    cin >> z;
+//    for (int y = 0; y < height; y++)
+//    {        
+//        for (int x = 0; x < width; x++)
+//        {
+//            if (x <= y || x - z >= y)
+//            {
+//                cout << " ";
+//            }
+//            else
+//            {
+//                cout << "*"; // blue triangles
+//            }        
+//        }
+//        cout << "\n";
+//    }
+//}
+
+//#include <iostream> // in color
 //#include <iostream>
 //#include <Windows.h>
 //using namespace std;
@@ -111,6 +166,68 @@ int main()
 //    }
 //}
 
+//#include <iostream>
+//#include <Windows.h>
+//using namespace std;
+//int main()
+//{
+//    int width = 35;
+//    int z = 3;
+//    int step;
+//    cout << "Enter number of steps - ";
+//    cin >> step;
+//    int height = 0;
+//    height = step * 2;
+//    //char symbol = 178;
+//    for (int y = 0; y < height; y++)
+//    {
+//        
+//        for (int x = 0; x < width; x++)
+//        {
+//            if (y % 2 != 0 && x % 2 != 0 || x <= y - 1 || x - z >= y)
+//            {
+//                cout << "*";
+//            }
+//            else
+//            {
+//                cout << " ";
+//            }
+//        {    
+//        cout << "\n";
+//    }
+//}
+
+//#include <iostream>
+//#include <Windows.h>
+//using namespace std;
+//int main()
+//{
+//    int width = 35;
+//    int z = 3;
+//    int step;
+//    cout << "Enter number of steps - ";
+//    cin >> step;
+//    int height = 0;
+//    height = step * 2;
+//    char symbol = 178;
+//    for (int y = 0; y < height; y++)
+//    {
+//        HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+//        for (int x = 0; x < width; x++)
+//        {
+//            if (y % 2 != 0 && x % 2 != 0 || x <= y - 1 || x - z >= y)
+//            {
+//                SetConsoleTextAttribute(h, 12); // light blue diagonals
+//            }
+//            else
+//            {
+//                SetConsoleTextAttribute(h, 9); // blue triangles
+//            }
+//            cout << symbol << symbol;
+//        }
+//        cout << "\n";
+//    }
+//}
 
 
 
@@ -126,52 +243,29 @@ int main()
 //using namespace std;
 //int main()
 //{
-//
-//    int width = 12;
+//    int width = 35;
 //    int height = 10;
+//    cout << "Enter the size of the step - ";
+//    int z;
+//    cin >> z;
+//    cout << "Enter the number step - ";
+//    cin >> height;
 //    char symbol = 178;
 //    for (int y = 0; y < height; y++)
 //    {
 //        HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-//
 //        for (int x = 0; x < width; x++)
-//        {
-//            
-//             if (x == y || x + 1 == y || x + 2 == y )
+//        {        
+//             if ( x <= y || x - z >= y)
 //            {
-//                 
-//                SetConsoleTextAttribute(h, 12); // light blue diagonals
-//
+//              SetConsoleTextAttribute(h, 12); // light blue diagonals
 //            }
-//           
-//            else
-//            {
-//                 cout << "  ";
+//             else
+//            {                
 //                SetConsoleTextAttribute(h, 9); // blue triangles
-//
 //            }
 //            cout << symbol << symbol;
 //        }
 //        cout << "\n";
-//        for (int x = 0; x < width; x++)
-//        {
-//
-//            if (x == y + 2 && x + 1 == y && x + 2 == y)
-//            {
-//                SetConsoleTextAttribute(h, 3); // light blue diagonals
-//            }
-//            if (x == y )
-//            { 
-//                cout << "  ";
-//                SetConsoleTextAttribute(h, 6); // light blue diagonals
-//            }
-//            else
-//            {
-//                SetConsoleTextAttribute(h, 9); // blue triangles
-//                
-//            }
-//            cout << symbol << symbol;
-//        }
-//        cout << " \n";
 //    }
-//}
+// }  
